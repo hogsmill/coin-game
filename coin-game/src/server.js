789ba289253144402eaa08eb80f36c1e460440c9
jjleight <jjleight@yahoo.co.uk>
2020-06-05 12:47:51 +0100

    [JL] tidied up the server.js slightly

--- coin-game/src/server.js
+++ coin-game/src/server.js
@@ -2,21 +2,19 @@ const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
-
 io.on("connection", (socket) => {
   console.log(`A user connected with socket id ${socket.id}.`);
 
-  socket.broadcast.emit("user-connected", socket.id);
+  // socket.broadcast.emit("user-connected", socket.id);
 
   socket.on("disconnect", () => {
     // socket.broadcast.emit("user-disconnected", socket.id);
     console.log(`User with socket id ${socket.id} has disconnected.`);
   });
 
-  socket.on("nudge-client", (data) => {
-    socket.broadcast.to(data.to).emit("client-nudged", data);
-  });
+  // socket.on("nudge-client", (data) => {
+  //   socket.broadcast.to(data.to).emit("client-nudged", data);
+  // });
 });
 
 http.listen(3000, () => {
