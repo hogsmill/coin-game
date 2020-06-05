5e12fa0622dc3cbd9846cf18dae4620fe1272416
jjleight <jjleight@yahoo.co.uk>
2020-06-05 21:55:23 +0100

    [JL] - updated the about section

--- coin-game/src/components/about/AboutView.vue
+++ coin-game/src/components/about/AboutView.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     <h3>The Coin Game</h3>
-    <a href="https://www.linkedin.com/pulse/coin-game-simple-effective-way-demonstrate-agility-steve-wells/">The Coin Game</a>
+    <p>
+      For more information about the coin game and how it can help change
+      mindset within teams that are new to agile, read this LinkedIn post:
+    </p>
+    <p>
+      <a
+        target="_blank"
+        href="https://www.linkedin.com/pulse/coin-game-simple-effective-way-demonstrate-agility-steve-wells/"
+        >The Coin Game</a
+      >
+    </p>
   </div>
 </template>
