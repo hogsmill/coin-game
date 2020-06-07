aca9cf8c5a8c998f9f71091fbb3fbaa54f253ec8
jjleight <jjleight@yahoo.co.uk>
2020-06-07 19:26:04 +0100

    [JL] updated about page text to be inline with agilesimulations.co.uk

--- coin-game/src/components/about/AboutView.vue
+++ coin-game/src/components/about/AboutView.vue
@@ -1,16 +1,18 @@
 <template>
-  <div>
-    <h3>The Coin Game</h3>
+  <div class="container">
+    <h1>The Coin Game</h1>
     <p>
-      For more information about the coin game and how it can help change
-      mindset within teams that are new to agile, read this LinkedIn post:
+      Explore different delivery strategies and see why delivering highest value
+      first is the best risk-reduction strategy.
     </p>
     <p>
+      See this
       <a
         target="_blank"
         href="https://www.linkedin.com/pulse/coin-game-simple-effective-way-demonstrate-agility-steve-wells/"
-        >The Coin Game</a
+        >LinkedIn blog post</a
       >
+      for a full description, and details of how to faciltate in person.
     </p>
   </div>
 </template>
