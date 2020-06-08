6b7f4b819c5e681fc690eee6184ba4661882a045
hogsmill <info@hogsmill.com>
2020-06-08 17:39:15 +0100

    correct deploy and connect

--- coin-game/src/server.js
+++ coin-game/src/server.js
@@ -1,21 +1,26 @@
 const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const os = require('os')
 
-var debugOn = true
+var prod = os.hostname() == "agilesimulations" ? true : false
+
+var connectDebugOff = prod
+var debugOn = !prod
+
 function emit(event, data) {
   if (debugOn) {
     console.log(event, data);
   }
-  io.emit(event, data);
+  io.emit(event, data)
 }
 
 io.on("connection", (socket) => {
-  console.log(`A user connected with socket id ${socket.id}.`);
+  connectDebugOff || console.log(`A user connected with socket id ${socket.id}.`)
 
   socket.on("disconnect", () => {
-    console.log(`User with socket id ${socket.id} has disconnected.`);
-  });
+    connectDebugOff || console.log(`User with socket id ${socket.id} has disconnected.`)
+  })
 
   socket.on("clickGo", (data) => { emit("goClicked", data) });
 });
