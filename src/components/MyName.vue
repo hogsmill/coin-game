b0ebcaaece360b2c7a643727b807b830b34c2911
jjleight <jjleight@yahoo.co.uk>
2020-07-19 21:23:58 +0100

    [JL] more styling tweaks to GameName and MyName

--- src/components/MyName.vue
+++ src/components/MyName.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="my-name float-right" v-if="!showAbout">
       <button class="btn btn-sm btn-secondary smaller-font" v-if="!myName" @click="show">Set My Name</button>
-      <span v-if="myName" @click="show" class="mr-2 mt-2 pointer">I am: {{myName}}</span>
+      <span v-if="myName" @click="show" class="mr-2 mt-2 pointer p-2 bg-light">I am: {{myName}}</span>
 
     <modal name="set-my-name" :height="120" :classes="['rounded', 'set-my-name']">
       <div class="float-right mr-2 mt-1">
