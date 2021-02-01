4434e6b737f81eaa72e4925602d842c1ba3b8de1
hogsmill <info@hogsmill.com>
2021-02-01 16:49:49 +0000

    update walk through

--- src/components/about/WalkThroughView.vue
+++ src/components/about/WalkThroughView.vue
@@ -7,7 +7,8 @@
     >
       Explain this for me...
     </button>
-    <modal name="walk-through" id="walk-through" :classes="['rounded']">
+
+    <modal name="walk-through" id="walk-through" :height="400" :classes="['rounded']">
       <div class="float-right mr-2 mt-1">
         <button type="button" class="close" @click="hide" aria-label="Close">
           <span aria-hidden="true">&times;</span>
@@ -17,10 +18,14 @@
         <h4>Welcome to the Coin Game</h4>
         <div>
           <p>
-            This simulation shows the importance of focussing on value delivery
+            This game demonstrates the importance of focussing on value delivery
             as the best means of satisfying customer need, and the best strategy
             for reducing risk
           </p>
+          <p>
+            Details on the physical game that is the basis of this one can be found
+            in <a href="https://www.linkedin.com/pulse/coin-game-simple-effective-way-demonstrate-agility-steve-wells/" target="blank">
+            this</a> LinkedIn post.
           <div class="coin-game-round" />
         </div>
       </div>
@@ -42,15 +47,53 @@
         </div>
       </div>
       <div class="mt-4" v-if="step == 3">
+        <h4>Playing The Game</h4>
+        <p>
+          Two demo games are already to set up if with to use them. Simply click the
+          <button class="btn btn-sm btn-secondary smaller-font">
+            Set Up Game
+          </button> button and select the game, either:
+        </p>
+        <ul>
+          <li>Workshop <em>(None) Single Team Game</em>, game <em>Demo</em> for a game with a
+              single team, or</li>
+          <li>Workshop <em>Demo</em>, and one of the teams from the drop down for a workshop
+              with multiple teams, to add some competition and comparison between teams</li>
+        </ul>
+        <p>
+          To set up your own game, using your teams' names and roles, use the facilitator tab
+          by adding <em>?host</em> to the URL:
+        </p>
+        <p class="center">
+          <a href="http://agilesimulations.co.uk/coin-game/?host">
+            http://agilesimulations.co.uk/coin-game/?host
+          </a>
+        </p>
+      </div>
+      <div class="mt-4" v-if="step == 4">
+        <h4>Playing The Game</h4>
+        <p>
+          Once the game is set up, you can optionally (but recommended..) click on the roles
+          to assign players to those roles as shown below:
+        </p>
+        <div class="role-select">
+          <div class="rounded selecting" />
+          <div class="rounded selected" />
+        </div>
+      </div>
+      <div class="mt-4" v-if="step == 5">
         <h4>Round 1: Batch Delivery</h4>
         <p>
           In this round, all coins must be played by a role before being passed
-          on <em>en masse</em>. This simulates a batch, or waterfall approach.
+          on <em>en masse</em>.
+        </p>
+        <p>
+          This simulates a batch, or waterfall approach.
           The round stops after {{ gameState.config.timeLimit.click }} seconds, and it is
           extremely unlikely that any value will reach the customer in this time.
         </p>
       </div>
-      <div class="mt-4" v-if="step == 4">
+      <div class="mt-4" v-if="step == 6">
         <h4>Round 2: Kanban Delivery</h4>
         <p>
           In this round, coins are passed on as soon as they are clicked. This
@@ -61,12 +104,12 @@
           deliver all the coins to the customer
         </p>
       </div>
-      <div class="mt-4" v-if="step == 5">
+      <div class="mt-4" v-if="step == 7">
         <h4>Round 3: Value Delivery</h4>
         <p>
           This round is also kanban (coins passed on immediately), but the timer
-          stops <em>after {{ gameState.config.valueTimeLimit.click }} seconds!</em>. Coins are click highest-value first,
-          however, and the amount of value delivered is recorded.
+          stops <em>after {{ gameState.config.valueTimeLimit.click }} seconds!</em>. Coins are
+          clicked highest-value first, however, and the amount of value delivered is recorded.
         </p>
         <p>
           Typically, despite having only one sixth of the time of round 1, most
@@ -76,7 +119,7 @@
         </p>
         <p>This is the light bulb moment...</p>
       </div>
-      <div class="mt-4" v-if="step == 6">
+      <div class="mt-4" v-if="step == 8">
         <h4>Game Play</h4>
         <p>
           Click the 'Play' buttons to play each round of the game as you would
@@ -85,31 +128,14 @@
           available to be played
         </p>
         <p>
+          Enjoy!
+        </p>
+        <!--
+        <p>
           The game can also be run in demo mode; if you're presenting to
           management or C-Suite, or doing an online presentation (<em>not currently available</em>)
         </p>
-      </div>
-      <div class="mt-4" v-if="step == 7">
-        <h4>Game Play</h4>
-        <p>
-          Click on the role in the header of this table to add the real names of
-          people playing the game, so you can see who's moving coins, and chivvy
-          them along accordingly...
-        </p>
-      </div>
-      <div class="mt-4" v-if="step == 8">
-        <h4>Play The Game</h4>
-        <p>
-          To play the game, all players need to set the same <b>Game Name</b>.
-        </p>
-        <p>
-          The current state of the game will then be updated in real time
-          whenever a coin is clicked so you can play the game as interactively as
-          if you were doing it round a table!
-        </p>
-        <p>
-          Enjoy!
-        </p>
+        -->
       </div>
       <div class="buttons" v-if="step < 8">
         <button class="btn btn-info" @click="incrementStep">
@@ -134,17 +160,7 @@ import params from '../../lib/params.js'
 export default {
   data() {
     return {
-      step: 1,
-      default: { width: 600, height: 260 },
-      positions: {
-        2: { height: 300 },
-        3: { height: 240 },
-        4: { height: 255 },
-        5: { height: 330 },
-        6: { height: 300 },
-        7: { height: 180 },
-        8: { height: 280 }
-      }
+      step: 1
     }
   },
   computed: {
@@ -166,15 +182,6 @@ export default {
     }
   },
   methods: {
-    setDefault() {
-      const elem = document.getElementsByClassName('vm--modal')[0].getBoundingClientRect()
-      this.default = {
-        top: elem.top,
-        left: elem.left,
-        width: elem.width,
-        height: elem.height
-      }
-    },
     show() {
       this.$modal.show('walk-through')
     },
@@ -189,42 +196,36 @@ export default {
       this.show()
     },
     incrementStep() {
-      if (this.step == 1) {
-        this.setDefault()
-      }
       this.step = this.step + 1
-      const elem = document.getElementsByClassName('vm--modal')[0]
-      let target, positions = {}
-      if (this.positions[this.step].target) {
-        target = document.getElementById(this.positions[this.step].target)
-        target = target.getBoundingClientRect()
-        positions.left = target.left + 30
-        positions.top = target.top + 30
-      } else {
-        positions = this.default
-      }
-      if (this.positions[this.step].width) {
-        positions.width = this.positions[this.step].width
-      }
-      if (this.positions[this.step].height) {
-        positions.height = this.positions[this.step].height
-      }
-      elem.style.left = positions.left + 'px'
-      elem.style.top = positions.top + 'px'
-      elem.style.width = positions.width + 'px'
-      elem.style.height = positions.height +'px'
     }
   },
 }
 </script>
 
-<style>
+<style lang="scss">
 .buttons {
   padding: 6px;
+  position: absolute;
+  bottom: 20px;
+  left: 228px;
 }
-#walk-through p {
-  margin-left: 8px;
-  margin-right: 8px;
+#walk-through {
+  p {
+    text-align: left;
+    margin: 0 24px 12px 24px;
+
+    &.center {
+      text-align: center;
+    }
+  }
+  ul {
+    margin-bottom: 12px;
+
+    li {
+      margin: 6px 24px 12px 36px;
+      text-align: left;
+    }
+  }
 }
 .coin-game-round {
   width: 500px;
@@ -250,5 +251,25 @@ export default {
 .value-graph {
   margin-left: 8px;
   background-image: url("../../assets/img/value.png");
+}
+
+.role-select {
+  margin: 0 auto;
+
+  div {
+    margin: 12px;
+    border: 1px solid #ccc;
+    height: 190px;
+    width: 200px;
+    display: inline-block;
+    box-shadow: 2px 2px 3px #aaa;
+
+    &.selecting {
+      background-image: url("../../assets/img/coin-game-role-select.jpg");
+    }
+    &.selected {
+      background-image: url("../../assets/img/coin-game-role-selected.jpg");
+    }
+  }
 }
 </style>
